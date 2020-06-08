82d4907bde421999d57be9e1641ee8d92f3c534f
Thiago Velho <trdvelho@yahoo.com.br>
2020-06-08 16:40:48 -0300

    README updated with instructions for Frontend SetUp, backend routes changed according to RESTFul

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,9 +5,9 @@ const TopicController = require('./controllers/TopicController');
 const routes = Router();
 
 routes.get('/topics', TopicController.index)                // Returns a list of all topics in the DB
-routes.post('/topic', TopicController.store);               // Creates a new topic in the DB
-routes.delete('/topic/:id', TopicController.destroy);       // Deletes an existing topic based on its _id
-routes.patch('/topic/:id', TopicController.updateTopic);    // Changes the name and description of a topic based on its _id
-routes.post('/topic/:id/voteup', TopicController.voteUp);       // Gives a vote up to a specific topic based on its _id
+routes.post('/topics', TopicController.store);               // Creates a new topic in the DB
+routes.delete('/topics/:id', TopicController.destroy);       // Deletes an existing topic based on its _id
+routes.patch('/topics/:id', TopicController.updateTopic);    // Changes the name and description of a topic based on its _id
+routes.post('/topics/:id/voteup', TopicController.voteUp);       // Gives a vote up to a specific topic based on its _id
 
 module.exports = routes;
