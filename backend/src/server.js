71ae46e8e3a2b56916676cd52daceb2f079ab3e1
Thiago Velho <trdvelho@yahoo.com.br>
2020-06-10 23:51:48 -0300

    added env file

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const cors = require('cors');
+require('dotenv').config();
+const { SERVER_HOST, SERVER_PORT, DB_HOST, DB_PORT } = process.env;
 
 const  mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 
-mongoose.connect('mongodb://localhost:27017/setuptalk', 
+mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/setuptalk`, 
     {user: 'setuptalker',pass: 'SetupTalker!0', useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(SERVER_PORT, SERVER_HOST);
+console.log(`Listening to ${SERVER_HOST} at port ${SERVER_PORT}`);
